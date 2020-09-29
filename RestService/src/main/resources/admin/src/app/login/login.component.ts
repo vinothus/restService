a20c3cb5ed93eab485d05b2864076ad7b1070682
Vinoth <Vinoth@Vinoth-PC>
2020-09-29 17:32:40 +0530

    login register validation

--- RestService/src/main/resources/admin/src/app/login/login.component.ts
+++ RestService/src/main/resources/admin/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup,Validators  } from "@angular/forms";
 
 import { AuthService } from '../auth/auth-service.service';
 
@@ -12,27 +12,39 @@ import { AuthService } from '../auth/auth-service.service';
 
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-
+  loading = false;
+  submitted = false;
   constructor(
     public formBuilder: FormBuilder,
     public authService: AuthService,
     public router: Router
   ) {
     this.loginForm= this.formBuilder.group({
-      email: [''],
-      password: ['']
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     })
   }
 
   ngOnInit() {
 	
-	
+	 this.loginForm= this.formBuilder.group({
+     email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: ['', Validators.required]
+    })
  }
 
+get f() { return this.loginForm.controls; }
+
   loginUser() {
+	 this.submitted = true;
+	console.log(this.loginForm.controls.email.errors);
+	//console.log(this.loginForm.controls.password.errors['required']);
 	 localStorage.setItem('access_token', 'trail')
+if (this.loginForm.invalid) {
+            return;
+        }
    //this.authService.login(this.loginForm.value)
     this.router.navigate(['dashboard']);
-
+  
   }
 }
--- RestService/src/main/resources/admin/src/app/register/register.component.ts
+++ RestService/src/main/resources/admin/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
 import { AuthService } from "../auth/auth-service.service";
 @Component({
@@ -9,24 +9,31 @@ import { AuthService } from "../auth/auth-service.service";
 })
 export class RegisterComponent implements OnInit {
 registerForm: FormGroup;
-  	
- 
-
+ loading = false;
+  submitted = false; 	
 constructor(
     public formBuilder: FormBuilder,
     public authService: AuthService,
     public router: Router
   ) {
     this.registerForm= this.formBuilder.group({
-      name: [''],
-      email: [''],
-      password: ['']
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: ['', Validators.required],
+	  phoneno:['', Validators.required],
+      address:['', Validators.required],
+      repassword:['',Validators.required]
     })
   }
 
   ngOnInit() { }
-
+get f() { return this.registerForm.controls; }
   registerUser() {
+	 this.submitted = true;
+if (this.registerForm.invalid) {
+            return;
+        }
+	
     this.authService.register(this.registerForm.value).subscribe((res) => {
       if (res.result) {
         this.registerForm.reset()
