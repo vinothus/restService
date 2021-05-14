15fff2efa9d0c431de9161e95bc9c72f444db3f2
Vinoth <Vinoth@Vinoth-PC>
2021-05-15 00:26:31 +0530

    FIX load Maps

--- RestService/src/main/resources/admin/src/app/maps/maps.component.ts
+++ RestService/src/main/resources/admin/src/app/maps/maps.component.ts
@@ -111,15 +111,15 @@ export class MapsComponent implements OnInit {
         }]
 
     };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ //   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: "Hello World!"
-    });
+ //   var marker = new google.maps.Marker({
+ //       position: myLatlng,
+ //       title: "Hello World!"
+ //   });
 
     // To add the marker to the map, call setMap();
-    marker.setMap(map);
+//    marker.setMap(map);
   }
 
 }
